0628d6ff825c74da23dd61115cd6d6238fcfbe29
Alex <zinchukalex@gmail.com>
2026-04-16 15:27:55 +0100

    change

--- NOTE.py
+++ NOTE.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-print(4 + 2)
-=======
 def sum_num(x:int|float, y:int|float):
     return x + y
     
@@ -8,4 +5,3 @@ print(sum_num(4, 2))
 print(sum_num(46, 25))
 print(sum_num(41, 23))
 print(sum_num(456, 23))
->>>>>>> 30f5fbe01080aee2a5757f10b1960a1fe2576a3a
